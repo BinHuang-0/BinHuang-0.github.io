779544f7644f29efa049889b202e6f90a4c69776
Bin <binhuang295@gmail.com>
2020-06-18 15:18:15 -0500

    Revamped the handling of the deck

--- Projects/tsuku.md
+++ Projects/tsuku.md
@@ -11,33 +11,33 @@ Still in Development
 
 <!-- Fill Deck -->
 
+
 <div class="left-half">
 
-<h2>Cards</h2>
+<h2>Add to Stack</h2>
 
 <ul>
-{% for item in site.data.oracleCards %}
+{% for item in site.data.ottStack %}
     <img src="{{item.img}}" width=75px onclick="addCard('{{item.name}}', '{{item.img}}')">
 {% endfor%}
 </ul>
 
+<button onclick="drawCard()">Draw</button>
+<button onclick="resetDeck()">Reset</button>
+
 </div>
  
 
-<!--
-    draw starting hand
-    draw
-    tell what cards you've drawn
-    search tsukuyomi
-    draw and replace
--->
 <div class="right-half">
 
 <h2>Deck</h2>
-<span id="cardCount">Deck Size: 0</span>
+<span id="cardCount">Deck Size: 40</span>
 <ul>
     <span id="deckPlace"></span>
 </ul>
-<button>Start</button>
 
 </div>
+
+<script> 
+    resetDeck();
+</script>
--- js/tsuku.js
+++ js/tsuku.js
@@ -1,18 +1,37 @@
 var deck = [];
 
-function addCard(name, img) {
-    if(deck.length < 40) {
-        var n = {
-            "name": name, "img": img
-        }
-        deck.push(n);
-
-        showDeck();
+function resetDeck() {
+    var n = {
+        "name": "Unknown", "img": "?"
     }
+    var l = deck.length;
+    for(var i = 0 ; i < l; i++) {
+        deck.shift();
+    }
+    for(var i = 0 ; i < 40; i++) {
+        deck.push(n);
+    }
+
+    showDeck();
+}
+
+function addCard(name, img) {
+    deck.shift();
+    var n = {
+        "name": name, "img": img
+    }
+    deck.push(n);
+
+    showDeck();
 }
 
 function removeCard(pos) {
     deck.splice(pos, 1);
+    
+    var n = {
+        "name": "Unknown", "img": "?"
+    }
+    deck.unshift(n);
 
     showDeck();
 }
@@ -20,7 +39,16 @@ function removeCard(pos) {
 function showDeck() {
     document.getElementById("cardCount").innerHTML = "Deck Size: " + deck.length + "<br>";
     document.getElementById("deckPlace").innerHTML = "";
-    for(var card in deck)
-        document.getElementById("deckPlace").innerHTML += "<img src=" + deck[card].img + " width=75px onclick='removeCard(" + card + ")'>";
- 
+    for(var card in deck) {
+        if(deck[card].img != "?")
+            document.getElementById("deckPlace").innerHTML += "<a onclick='removeCard(" + card + ")'><img src=" + deck[card].img + " width=20px> " + deck[card].name + "</a><br>";
+        else
+            document.getElementById("deckPlace").innerHTML += deck[card].img + " " + deck[card].name + "<br>";
+    }
+}
+
+function drawCard() {
+    deck.shift();
+
+    showDeck();
 }
